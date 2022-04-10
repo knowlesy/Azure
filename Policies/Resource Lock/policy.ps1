3cf6ba2da14a29cf650fc24768c46338df926858
knowlesy <rockpeteuk@gmail.com>
2022-04-10 13:12:09 +0100

    update to policy now just based on resources with tags

--- Policies/Resource Lock/policy.ps1
+++ Policies/Resource Lock/policy.ps1
@@ -1,6 +1,7 @@
 #Ref
 #https://docs.microsoft.com/en-gb/azure/governance/policy/how-to/get-compliance-data#on-demand-evaluation-scan---azure-powershell
 #https://www.stefanroth.net/2020/02/03/azure-policy-audit-and-deploy-cannotdelete-lock-on-resource-group-based-on-tags/
+#https://www.reddit.com/r/AZURE/comments/nehn35/can_policy_apply_locks_to_resources_and/
 #https://docs.microsoft.com/en-us/azure/azure-resource-manager/management/lock-resources?tabs=json#arm-template
 ##TAKE NOTE OF THE CONSIDERATIONS
 
